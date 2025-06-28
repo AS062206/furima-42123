e49ee271e54b6e956c0ee303e61ca0887f7951f6
AS062206 <lucky.myu8.29@gmail.com>
2025-06-28 13:17:43 +0900

    java 金額の計算

---
+++ app/javascript/item_price.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const priceInput = document.getElementById('item-price')
+  const commissionDisplay = document.getElementById('add-tax-price')
+
+  priceInput.addEventListener('input', () => {
+    const price = parseInt(priceInput.value, 10)
+    if (isNaN(price) || price < 300 || price > 9999999) {
+      commissionDisplay.textContent = '0'
+      profitDisplay.textContent = '0'
+      return
+    }
+
+    const commission = Math.floor(price * 0.1)
+    const profit = price - commission;
+
+    commissionDisplay.textContent = commission.toLocaleString()
+    profitDisplay.textContent = profit.toLocaleString()
+  })
+})
