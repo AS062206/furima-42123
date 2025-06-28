0e508f9891fa8ee3e4a06424bdbe91cfbab4dd01
AS062206 <lucky.myu8.29@gmail.com>
2025-06-28 10:29:47 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  private
+  
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
