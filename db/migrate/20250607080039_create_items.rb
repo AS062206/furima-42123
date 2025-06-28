d5b210d22764422f8085e29e441e57f9feb905f8
AS062206 <lucky.myu8.29@gmail.com>
2025-06-28 10:38:02 +0900

    コードミス修正

--- db/migrate/20250607080039_create_items.rb
+++ db/migrate/20250607080039_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-      create_table :items do |t|
       t.string :name, null: false
       t.text :description, null: false
       t.integer :category_id, null: false
