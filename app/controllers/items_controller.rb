1d321f917b1dde367968caa39302648e4f4814d9
AS062206 <lucky.myu8.29@gmail.com>
2025-06-28 12:08:21 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+
+    @categories = Category.all
+    @conditions = Condition.all
+    @shipping_fees = ShippingFee.all
+    @prefectures = Prefecture.all
+    @shipping_durations = ShippingDuration.all
   end
 
   def create
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
