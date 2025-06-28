0dbeffd6aa6cfa48bbb1d6a444ad070d4cbc8fea
AS062206 <lucky.myu8.29@gmail.com>
2025-06-28 12:26:01 +0900

    出品情報の新規登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
 
@@ -14,12 +15,34 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      @categories = Category.all
+      @conditions = Condition.all
+      @shipping_fees = ShippingFee.all
+      @prefectures = Prefecture.all
+      @shipping_durations = ShippingDuration.all
+      render :new, status: :unprocessable_entity
+    end
+
   end
 
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_fee_id,
+      :prefecture_id,
+      :shipping_duration_id,
+      :price,
+      :image
+    ).merge(user_id: current_user.id)
   end
-
 end
