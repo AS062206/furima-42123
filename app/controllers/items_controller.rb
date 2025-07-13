a6776db9172df15eb1e8df6a932e8b9d5c99b189
AS062206 <lucky.myu8.29@gmail.com>
2025-07-13 13:47:14 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -31,10 +32,42 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+
+    @categories = Category.all
+    @conditions = Condition.all
+    @shipping_fees = ShippingFee.all
+    @prefectures = Prefecture.all
+    @shipping_durations = ShippingDuration.all
+  end
+
+  def update
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      @categories = Category.all
+      @conditions = Condition.all
+      @shipping_fees = ShippingFee.all
+      @prefectures = Prefecture.all
+      @shipping_durations = ShippingDuration.all
+      render :edit
+    end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(
